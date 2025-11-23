17b7eca0560bb5ccb1d3a58b6277ebf5f83bdb20
nischaydeshmukh-coder <nischaydeshmukh@gmail.com>
2025-11-23 19:10:12 +0530

    Add files via upload

---
+++ Assignments/Assignment_29/program29_4.java
@@ -0,0 +1,66 @@
+//Input :  iRow = 5  iCol = 5
+//Output :
+//          1   2   3   4   5 
+//         -1  -2  -3  -4  -5
+//          1   2   3   4   5 
+//         -1  -2  -3  -4  -5
+//          1   2   3   4   5
+
+/////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description :   Display the given pattern.
+//  Input :         int,int
+//  Output :        pattern
+//  Auther :        Nischay Hemant Deshmukh
+//  Date :          23/11/2025
+//
+/////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class Pattern 
+{
+    public void Display(int iRow, int iCol)
+    { 
+        int i = 0, j = 0;
+        
+        int iNo = 1 ;
+    
+        for(i = 1; i <= iRow; i++)
+        {  
+            for(j = 1,iNo = 1;j <= iCol; j++,iNo++)
+            {
+                if(i % 2 == 0 )
+                {
+                    System.out.print(-iNo+"\t");  
+                }
+                else
+                {
+                    System.out.print(iNo+"\t");
+                    
+                } 
+            }
+            System.out.println();
+        }  
+    } 
+}
+class program29_4
+{
+    public static void main(String[] args) 
+    { 
+        int iValue1 = 0, iValue2 = 0; 
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of Rows:"); 
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Cols:"); 
+        iValue2 = sobj.nextInt();
+    
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2); 
+    
+         
+    } 
+}
