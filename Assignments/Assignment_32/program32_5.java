ccc985ec748c77b3ed2ef58c0cbf5c13efe7be1e
nischaydeshmukh-coder <nischaydeshmukh@gmail.com>
2025-11-24 22:06:09 +0530

    Add files via upload

---
+++ Assignments/Assignment_32/program32_5.java
@@ -0,0 +1,69 @@
+//Input :  iRow = 5  iCol = 5
+//Output :
+//          1   2   3   4   5   
+//          1   2           5
+//          1       3       5
+//          1           4   5
+//          1   2   3   4   5
+
+/////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description :   Display the given pattern.
+//  Input :         int,int
+//  Output :        pattern
+//  Auther :        Nischay Hemant Deshmukh
+//  Date :          24/11/2025
+//
+/////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class Pattern 
+{
+    public void Display(int iRow, int iCol)
+    { 
+        int i = 0, j = 0, iNo =0;
+        
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Input");
+            System.out.println("Row Number and Column number should be same");
+            return;
+        }
+        for(i = 1; i <= iRow; i++)
+        {  
+            for(j = 1,iNo =1 ;j <= iCol; j++, iNo++)
+            {
+                if(i ==1 || i == iRow || j == 1 || j == iCol || i == j )
+                {
+                    System.out.print(iNo+"\t");
+                }
+                else
+                {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }  
+    } 
+}
+class program32_5
+{
+    public static void main(String[] args) 
+    { 
+        int iValue1 = 0, iValue2 = 0; 
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of Rows:"); 
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Cols:"); 
+        iValue2 = sobj.nextInt();
+    
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2); 
+    
+         
+    } 
+}
