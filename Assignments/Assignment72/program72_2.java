c5490b1f3e4d74e579feeb7ebe68c72d3e07b9a2
Niscahy Hemant Deshmukh <nischaydeshmukh@gmail.com>
2026-02-23 00:35:17 +0530

    Add files via upload

---
+++ Assignments/Assignment72/program72_2.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class Matrix
+{
+    public int iRow ;
+    public int iCol ;
+    public int Arr[][];
+
+    public Matrix(int a, int b)
+    {
+        this.iRow = a;
+        this.iCol = b;
+
+        Arr = new int[iRow][iCol];
+    }
+    public void Accept()
+    {
+        int i =0 , j = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of matrix ");
+
+        for(i = 0; i < iRow ; i++)
+        {
+            System.out.println("Enter the elements of row : "+(i+1));
+
+            for(j = 0 ; j < iCol ; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+
+        sobj.close();
+    }
+
+    public void Display()
+    {
+        int i =0 , j = 0; 
+
+        System.out.println("Elements of matrix are : ");
+
+        for(i = 0; i < iRow ; i++)
+        {
+            for(j = 0 ; j < iCol ; j++)
+            {
+                System.out.print(Arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public int Frequency(int iNo)
+    {
+        int i =0 , j = 0; 
+        int iCount = 0;
+
+        for(i = 0; i < iRow ; i++)
+        {
+            for(j = 0 ; j < iCol ; j++)
+            {
+                if(Arr[i][j] == iNo)
+                {
+                    iCount++;
+                }
+            }
+        }
+        return iCount;
+    }  
+}
+
+class program72_2
+{
+    public static void main(String[] args) 
+    {
+        int iRet = 0;
+        Matrix mobj = new Matrix(4,4);
+        mobj.Accept();
+        mobj.Display();
+
+        iRet = mobj.Frequency(5);
+        System.out.println("Frequency of element is : " +iRet);
+    }
+}
